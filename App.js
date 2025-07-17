b2707cdcd602fd54dc03a43f2c728a4cb97dbb7e
JRSLGD <jorisgbelia@gmail.com>
2025-07-17 16:11:22 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,8 @@
+//DOM
+
+const allbtns = [...document.querySelectorAll('.boutton')];
+const listKeycode = touches.map(touche => touche.dataset.key);
+
+document.addEventListener('keydown', (e) => {
+    const valeur = e.keyCode.toString();
+})
